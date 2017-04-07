21b1ad4fbdf5cadd1ccc9ff3a453d3e2fc0f1fd3
RohitMe <rohitmehta1996@gmail.com>
2017-04-07 08:41:42 +0530

    Add bar graph
    Now one can see report in both the formats: Pie chart as well as Bar graph

--- front.php
+++ front.php
@@ -1,18 +1,20 @@
 
 <?php
+
  require("DBConfig.php");
- //While linking please remove these comments 
-//	if(isset($_GET['submit'])){
+ //	if(isset($_GET['submit'])){
 //		$from = $_GET['From'];
 //		$to = $_GET['To'];
 //		$Fname = $_GET["name"];
 //		$UID = 2;
 		$from = '2017/02/19';
 		$to = '2017/03/02';
-		$Fname = 'Nikunj';
-		$sql = "SELECT t.ID
-				FROM tuserinfromations t
-				WHERE t.FullName = '$Fname' ";
+		$Fname = 'A';
+	//	$Pname = 'product1';
+		$sql = "SELECT t.TeamLeadID
+				FROM tSalesTeamInformation t
+				WHERE t.TeamName = '$Fname' ";
+				
 				$result  = mysqli_query($conn,$sql);
 				$count = mysqli_num_rows($result);
 		if($count == 0 )
@@ -22,17 +24,16 @@
 		else
 		{
 				$row=mysqli_fetch_array($result);
-				$UID=$row['ID'];
-		
-		
-		
-		
- $sql = "SELECT sum(a.Total) as T 
+				$UID=$row['TeamLeadID'];
+				
+				
+		$sql = "SELECT sum(a.Total) as T 
 		FROM (SELECT p.ProductCode ,sum(p.SalesCount) as Total
-				FROM tproductsalesdataprocessed p , tproductsalesdatasubmitted s
-				WHERE p.submitionID = s.ID and s.Submittedfor = '$UID'
-				and s.SubmittedDate between '$from' and '$to'
-				GROUP BY p.ProductCode)a";
+			FROM tproductsalesdataprocessed p , tproductsalesdatasubmitted s , tUserReportingHierarchy t
+			WHERE p.submitionID = s.ID 
+			and s.ID = t.MembersUserID
+			and t.LeadsUserID = '$UID'
+			GROUP BY p.ProductCode)a";
 		$result  = mysqli_query($conn,$sql);
 		$row=mysqli_fetch_array($result);
 		$total = $row['T'];	
@@ -46,7 +47,7 @@
  <title>
 Pie chart
  </title>
- <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+ <script type="text/javascript" src="http://www.google.com/jsapi"></script>
  <script type="text/javascript">
  google.load("visualization", "1", {packages:["corechart"]});
  google.setOnLoadCallback(drawChart);
@@ -58,10 +59,12 @@ Pie chart
 
 		
  $query = "SELECT p.ProductCode ,sum(p.SalesCount) as Total
-		FROM tproductsalesdataprocessed p , tproductsalesdatasubmitted s
-		WHERE p.submitionID = s.ID and s.Submittedfor = '$UID'
-		and s.SubmittedDate between '$from' and '$to'
-		GROUP BY p.ProductCode";
+FROM tproductsalesdataprocessed p , tproductsalesdatasubmitted s , tUserReportingHierarchy t
+WHERE p.submitionID = s.ID 
+and s.ID = t.MembersUserID
+and t.LeadsUserID = '$UID'
+and s.SubmittedDate between '$from' and '$to'
+GROUP BY p.ProductCode";
 
  $exec = mysqli_query($conn,$query);
  while($row = mysqli_fetch_array($exec)){
@@ -72,7 +75,7 @@ Pie chart
  ]);
 
  var options = {
- title: 'Member',
+ title: 'Product',
  colors: [<?php
  
 	$total;
@@ -99,13 +102,65 @@ Pie chart
  chart.draw(data, options);
  }
  </script>
+ <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script type="text/javascript">
+    google.charts.load("current", {packages:['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ["Element", "Density", { role: "style" } ],<?php 
+
+		
+ $query = "SELECT p.ProductCode ,sum(p.SalesCount) as Total
+FROM tproductsalesdataprocessed p , tproductsalesdatasubmitted s , tUserReportingHierarchy t
+WHERE p.submitionID = s.ID 
+and s.ID = t.MembersUserID
+and t.LeadsUserID = '$UID'
+and s.SubmittedDate between '$from' and '$to'
+GROUP BY p.ProductCode";
+
+ $exec = mysqli_query($conn,$query);
+ while($row = mysqli_fetch_array($exec)){
+if(($row['Total']/$total)*100  > 60)
+			$c = "blue";
+		elseif(($row['Total']/$total)*100  > 40)
+			$c =  "green";
+		else
+			$c =  "red";
+ echo "['".$row['ProductCode']."',".$row['Total'].","."'".$c."'"."],";
+ }
+ ?>
+ ]);
+
+      var view = new google.visualization.DataView(data);
+      view.setColumns([0, 1,
+                       { calc: "stringify",
+                         sourceColumn: 1,
+                         type: "string",
+                         role: "annotation" },
+                       2]);
+      var options = {
+        title: "",
+        width: 600,
+        height: 400,
+        bar: {groupWidth: "95%"},
+        legend: { position: "none" },
+      };
+      var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
+      chart.draw(view, options);
+  }
+  </script>
+
 </head>
 <body>
  <h3>Pie Chart</h3>
  <div id="piechart" style="width: 900px; height: 500px;"></div>
+ <h3>Bar Chart</h3>
+ <div id="columnchart_values" style="width: 900px; height: 300px;"></div>
 </body>
 </html>
 <?php
-		}
 
+
+		}
 ?>
