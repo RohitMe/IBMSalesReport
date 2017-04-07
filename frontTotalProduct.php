22d9e1cd4701e8e4ebb9abaed8bfd60bf3016357
RohitMe <rohitmehta1996@gmail.com>
2017-04-07 09:56:18 +0530

    Details of every product

--- frontTotalProduct.php
+++ frontTotalProduct.php
@@ -110,10 +110,60 @@ GROUP BY p.ProductCode";
  chart.draw(data, options);
  }
  </script>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script type="text/javascript">
+    google.charts.load("current", {packages:['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ["Element", "Density", { role: "style" } ],<?php 
+
+		
+ $query = "SELECT p.ProductCode ,sum(p.SalesCount) as Total
+FROM tproductsalesdataprocessed p , tproductsalesdatasubmitted s , tUserReportingHierarchy t
+WHERE p.submitionID = s.ID 
+and s.ID = t.MembersUserID
+and t.LeadsUserID = '$UID'
+and s.SubmittedDate between '$from' and '$to'
+GROUP BY p.ProductCode";
+
+ $exec = mysqli_query($conn,$query);
+ while($row = mysqli_fetch_array($exec)){
+if(($row['Total']/$total)*100  > 60)
+			$c = "blue";
+		elseif(($row['Total']/$total)*100  > 40)
+			$c =  "green";
+		else
+			$c =  "red";
+ echo "['".$row['ProductCode']."',".$row['Total'].","."'".$c."'"."],";
+ }
+ ?>
+ ]);
+
+      var view = new google.visualization.DataView(data);
+      view.setColumns([0, 1,
+                       { calc: "stringify",
+                         sourceColumn: 1,
+                         type: "string",
+                         role: "annotation" },
+                       2]);
+      var options = {
+        title: "",
+        width: 600,
+        height: 400,
+        bar: {groupWidth: "95%"},
+        legend: { position: "none" },
+      };
+      var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
+      chart.draw(view, options);
+  }
+  </script>
 </head>
 <body>
  <h3>Pie Chart</h3>
  <div id="piechart" style="width: 900px; height: 500px;"></div>
+   <h3>Bar Chart</h3>
+ <div id="columnchart_values" style="width: 900px; height: 300px;"></div>
 </body>
 </html>
 <?php
